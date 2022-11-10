02b30c7eb9153b10e2ba03adbdb09d54f4ec68cd
Jisu Kim <jisu891114@hotmail.com>
2022-11-10 11:11:32 +0900

    hw3

--- hw3/hw3_JisuKim.py
+++ hw3/hw3_JisuKim.py
@@ -2,6 +2,8 @@ import pybullet as p
 import pybullet_data
 import time
 import os
+import math
+import numpy as np
 
 # Environment setup
 SAMPLING_RATE = 1e-2    # 0.01s = 10 ms
@@ -14,28 +16,37 @@ p.setGravity(0, 0, -9.81)
 # Setup plane
 plane_id = p.loadURDF("plane.urdf")
 
+p.setAdditionalSearchPath(os.path.dirname(__file__) + '/ur_description')
 # Setup objects
 StartPos = [0, 0, 0]
 StartOrientation = p.getQuaternionFromEuler([0,0,0])
-manipulator_id = p.loadURDF("./ur_description/urdf/ur5.urdf", StartPos, StartOrientation, useFixedBase=1)
+robot_id = p.loadURDF("urdf/ur5.urdf", StartPos, StartOrientation, useFixedBase=1,flags=p.URDF_USE_SELF_COLLISION|p.URDF_USE_INERTIA_FROM_FILE)
 
-numJoints = p.getNumJoints(manipulator_id) 
-print("Number of joints: {}".format(numJoints))
+dof = p.getNumJoints(robot_id)-1
+joints = range(dof)
+# print("Number of joints: {}".format(numJoints))
 
 # Reset states controller
-p.resetJointState(manipulator_id, 0, targetValue=0)
-p.resetJointState(manipulator_id, 1, targetValue=0)
-p.setJointMotorControlArray(bodyUniqueId=manipulator_id,
+p.resetJointState(robot_id, 0, targetValue=0)
+p.resetJointState(robot_id, 1, targetValue=0)
+p.setJointMotorControlArray(bodyUniqueId=robot_id,
                             jointIndices=range(2),
                             controlMode=p.POSITION_CONTROL,
                             forces=[0. for _ in range(2)])
 
 # Perform simulation step
 while True:
+    joint_states = p.getJointStates(robot_id, joints)
+    pos = [state[0] for state in joint_states]
+    vel = [state[1] for state in joint_states]
+    
+    pos_ee, ori_ee, _, _, _, _ = p.getLinkState(robot_id,5)
+    
+    M_sim = np.array(p.calculateMassMatrix(robot_id, pos[:5]))
+    print(M_sim)
+    print()
     p.stepSimulation()
     time.sleep(SAMPLING_RATE)
-    cubePos, cubeOrn = p.getBasePositionAndOrientation(manipulator_id)
-
 # # Exit Simulation
 p.disconnect()
 print("Simulation end")
